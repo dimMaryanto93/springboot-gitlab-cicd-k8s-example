ed1f23659cfadeb0dfe142c696aa1e32a4afe876
dimasm93 <software.dimas_m@icloud.com>
2021-04-30 13:10:30 +0700

    init project springboot

---
+++ src/main/java/com/maryanto/dimas/example/SpringbootK8sExampleApplication.java
@@ -0,0 +1,13 @@
+package com.maryanto.dimas.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootK8sExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootK8sExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maryanto/dimas/example/SpringbootK8sExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maryanto.dimas.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootK8sExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
