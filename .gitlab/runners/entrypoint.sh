921ba4d28cda1b167d1486d153cb536d0181dea1
dimasm93 <software.dimas_m@icloud.com>
2021-07-19 19:15:22 +0700

    update using envsubst

--- .gitlab/runners/entrypoint.sh
+++ .gitlab/runners/entrypoint.sh
@@ -3,10 +3,11 @@ set -e
 export WORKING_DIRECTORY=src/main/kubernetes
 export DEPLOYMENT_FILE=$WORKING_DIRECTORY/deployment.yaml
 export SERVICE_FILE=$WORKING_DIRECTORY/service.yaml
+export DIRECTORY_OUTPUT=.k8s
 
-envsubst < DEPLOYMENT_FILE > deployment.yaml && \
-cat deployment.yaml
-envsubst < SERVICE_FILE > service.yaml && \
-cat service.yaml
+envsubst < $DEPLOYMENT_FILE > $DIRECTORY_OUTPUT/deployment.yaml && \
+cat $DIRECTORY_OUTPUT/deployment.yaml && \
+envsubst < $SERVICE_FILE > $DIRECTORY_OUTPUT/service.yaml && \
+cat $DIRECTORY_OUTPUT/service.yaml
 
 exec "$@"
