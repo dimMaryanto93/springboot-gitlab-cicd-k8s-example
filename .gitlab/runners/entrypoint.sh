ee78ecf4b4d3abc3c2c27d411a45a5101d9bd40b
Dimas Maryanto <software.dimas_m@icloud.com>
2021-08-02 09:37:27 +0000

    public docker image by repository name and namespace

--- .gitlab/runners/entrypoint.sh
+++ .gitlab/runners/entrypoint.sh
@@ -4,6 +4,7 @@ export WORKING_DIRECTORY=src/main/kubernetes
 export DEPLOYMENT_FILE=$WORKING_DIRECTORY/deployment.yaml
 export SERVICE_FILE=$WORKING_DIRECTORY/service.yaml
 export DIRECTORY_OUTPUT=.k8s
+export CI_NODE_PORT_EXPOSE=$(printf "3%04d" $CI_PROJECT_ID)
 
 envsubst < $DEPLOYMENT_FILE > $DIRECTORY_OUTPUT/deployment.yaml && \
 cat $DIRECTORY_OUTPUT/deployment.yaml && \
